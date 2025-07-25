f5cd10b67822665370a16aef58cbba3c0666ca66
Diego-ACC <A01645484@tec.mx>
2025-07-25 15:22:49 -0600

    Puerto único para Railway

--- indexCASEWORLD.js
+++ indexCASEWORLD.js
@@ -3,7 +3,7 @@ express = require('express');
 const db = require('./db');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 1980;
+const port = process.env.PORT;
 
 
 app.use(cors());
